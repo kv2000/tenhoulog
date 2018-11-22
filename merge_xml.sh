81d81a8ca0de9972bd628c064d0a629d09584780
lietxia <lietxia@gmail.com>
2018-11-22 18:51:47 +0800

    x

---
+++ merge_xml.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cat xml_split/xml2009.tar.gz.* > xml2009.tar.gz;
+cat xml_split/xml2010.tar.gz.* > xml2010.tar.gz;
+cat xml_split/xml2011.tar.gz.* > xml2011.tar.gz;
+cat xml_split/xml2012.tar.gz.* > xml2012.tar.gz;
+cat xml_split/xml2013.tar.gz.* > xml2013.tar.gz;
+cat xml_split/xml2014.tar.gz.* > xml2014.tar.gz;
+cat xml_split/xml2015.tar.gz.* > xml2015.tar.gz;
+cat xml_split/xml2016.tar.gz.* > xml2016.tar.gz;
+cat xml_split/xml2017.tar.gz.* > xml2017.tar.gz;
---
+++ php_download/downtenhou2009.php
@@ -0,0 +1,47 @@
+<?php
+function downlog($paipu)
+{
+    $ch = curl_init('http://e.mjv.jp/0/log/?' . $paipu);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept-Encoding: gzip, deflate, sdch', 'Referer: tenhou.net'));
+    curl_setopt($ch, CURLOPT_TIMEOUT, 12);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
+    $a = curl_exec($ch);
+    curl_close($ch);
+    if (preg_match('/<a[^>]+href="([^"]+)"/', $a, $matches)) {
+        $ch = curl_init($matches[1]);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 12);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept-Encoding: gzip, deflate, sdch', 'Referer: tenhou.net'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
+        $a = curl_exec($ch);
+        curl_close($ch);
+    }
+    return $a;
+}
+$year=2009;
+$list = scandir("./log$year/");
+array_shift($list);
+array_shift($list);
+$pattern = '/log=([^"]+)/'; //需要转义/
+//$urls = array();
+foreach ($list as $filename) {
+    $myfile = fopen("./log$year/" . $filename, "r") or die("Unable to open file!");
+    $text = fread($myfile, filesize("./log$year/" . $filename));
+    fclose($myfile);
+    preg_match_all($pattern, $text, $matches);
+    //$urls = array_merge($urls, $matches[1]);
+    foreach ($matches[1] as $paipu) {
+        if (!file_exists("./xml$year/" . $paipu . ".txt")) {
+            $xml = downlog($paipu);
+            if (strlen($xml) > 50) {
+                $myfile = fopen("./xml$year/" . $paipu . ".txt", "w");
+                fwrite($myfile, $xml);
+                fclose($myfile);
+            }
+        }
+    }
+}
+
+
+
